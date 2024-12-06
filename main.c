aa30c5058f87ee4eb802307882b11cc504a023db
juandi <jdcoto955@gmail.com>
2024-12-05 20:08:06 -0600

    Se crea la estrucruta Person

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Definición de la estructura Person
+typedef struct {
+    char name[50];
+    int age;
+    double height;
+} Person;
 
